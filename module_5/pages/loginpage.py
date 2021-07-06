5d9b210c50bef345f26c47fceb3b897b496b7f79
Ksenia Letyushova <letushovaksenia89@gmail.com>
2021-07-06 15:55:58 +0400

    new loginpage.py

--- module_5/pages/loginpage.py
+++ module_5/pages/loginpage.py
@@ -1,5 +1,5 @@
-from .base_page import BasePage
-
+from .basepage import BasePage
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert  'login' in self.browser.current_url
+        assert 'login' in self.browser.current_url
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
